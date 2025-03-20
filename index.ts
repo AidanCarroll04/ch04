8f3a206cf29e3f2ee37be70a66c481dc78f5b7e3
Aidan Carroll <aidancarroll2004@gmail.com>
2025-03-20 12:49:14 -0400

    add weather

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { routeHello, routeAPINames, routeWeather } from "./routes.js";
 import express, { Request, Response } from "express";
 
+import path from "path";
+
 const server = express();
 const port = 3000;
 
@@ -9,15 +11,17 @@ server.get('/hello', function (_req: Request, res: Response): void {
 	res.send(response);
 });
 
-server.get("/api/names", async function (_req: Request, res: Response): Promise<void> {
-	let response: string;
-	try {
-		response = await routeAPINames();
-		res.send(response);
-	}catch(err){
-		console.log(err)
+server.get("/api/names", 
+	async function (_req: Request, res: Response): Promise<void> {
+		let response: string;
+		try {
+			response = await routeAPINames();
+			res.send(response);
+		}catch(err){
+			console.log(err)
+		}
 	}
-});
+);
 
 server.get(
 	"/api/weather/:zipcode",
@@ -26,6 +30,12 @@ server.get(
 		res.send(response);
 	}
 );
+
+server.get("/components/weather", function (req: Request, res: Response): void {
+	const filePath = path.join(process.cwd(), "public", "weather.html");
+	res.setHeader("content-Type", "test/html");
+	res.sendFile(filePath);
+});
 
 server.listen(port, function ():void {
 	console.log('Listening on ' + port);
